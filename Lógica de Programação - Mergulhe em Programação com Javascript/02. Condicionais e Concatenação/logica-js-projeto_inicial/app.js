741d3ab3ebffe481cd1117bf404e204f00a56f73
Diego Rufino <128259891+mininoterraria@users.noreply.github.com>
2025-12-14 22:40:14 -0300

    Add files via upload

---
+++ Lógica de Programação - Mergulhe em Programação com Javascript/02. Condicionais e Concatenação/logica-js-projeto_inicial/app.js
@@ -0,0 +1,49 @@
+//OBSERVAÇÃO: REMOVER COMENTÁRIOS MULTI-LINHAS DE CADA ALTERNATIVA SE QUISER EXECUTAR.
+
+//01. Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+/*
+let diaSemana = prompt("Qual é o dia da semana?");
+
+if(diaSemana === 'Sábado' || diaSemana === 'Domingo'){
+    console.log("Bom fim de semana!")
+}else{
+    console.log("Boa semana!");
+}
+*/
+
+//02. Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+/*
+let numero = prompt("Insira um número: ");
+
+if(numero >= 0 && numero.length > 0){
+    console.log("O número é positivo.");
+}else if(numero < 0 && numero.length > 0){
+    console.log("O número é negativo.");
+}else{
+    console.log("Isso definitavamente não é um número.");
+}
+*/
+
+//03. Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+/*
+let pontuacao = 120;
+
+if(pontuacao >= 100){
+    console.log("Parabéns, você venceu!");
+}else{
+    console.log("Tente novamente para ganhar.");
+}
+*/    
+
+//04. Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+/*
+let saldo = 1000;
+console.log(`O valor do saldo é R$ ${saldo}`);
+*/
+
+//05. Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+/*
+let nome = prompt("Insira seu nome:");
+
+alert(`Boas vindas, ${nome}`);
+*/
